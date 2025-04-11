5e92a95b063cef69c64181478ea337afd69310a9
Avtar <Avtar92749@gmail.com>
2025-04-11 18:03:43 +0530

    add routing

--- hotel-bookings/src/App.jsx
+++ hotel-bookings/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Booking";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import Cabins from "./pages/Cabins";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>Hello</div>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            {/* <Route index element={<Navigate replace to="dashboard" />} /> */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ hotel-bookings/src/pages/Cabins.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cabins = () => {
+  return <div>cabins</div>;
+};
+
+export default Cabins;
