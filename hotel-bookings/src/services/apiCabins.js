df9956c660b261369a06a1f708f4def936da7ffd
navpreet singh <avtar92749@gmail.com>
2025-04-14 01:23:24 +0530

    toast

--- hotel-bookings/src/services/apiCabins.js
+++ hotel-bookings/src/services/apiCabins.js
@@ -9,3 +9,14 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function deleteCabin(id) {
+  // eq is filter method provided by supabase and data and error we are getting from supabase
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return data;
+}
